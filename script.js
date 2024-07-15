31e064442853033bc2f2c2e9da5b6bba88a3fe35
Mohamed Nawfal <mohamednawfal183@gmail.com>
2024-07-15 17:28:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+1.
+// let i;
+// for(i=1;i<=100;i++)
+// {
+//     if(i%3==0  && i%5==0)
+//     {
+//         document.write("FizzBuzz<br>");
+//     }
+//     else if(i%3==0)
+//     {
+//         document.write("Fizz<br>");
+//     }
+//     else if(i%5==0)
+//     {
+//         document.write("Buzz<br>");
+//     }
+//     else
+//     {
+//         document.write(i)
+//     }
+// }
+// document.write(i);
+
+2.
+
+    // let str1="malayalam";
+    // let str2=str1.split("").reverse().join("");
+    // if(str1==str2)
+    // {
+    //     document.write("Palindrome");
+    // }
+    // else
+    // {
+    //     document.write("Not a palindrome");
+    // }
+
+3.
+
+    // let arr=[10,50,90,40]
+    // let arr1=arr.sort()
+    // document.write(arr1[(arr1.length)-1]);
+
+
+4.
+
+    // function temp(c){
+    //     f=c*(9/5)+32;
+    //     document.write(c+"="+f);
+    // }
+    // temp(100)
